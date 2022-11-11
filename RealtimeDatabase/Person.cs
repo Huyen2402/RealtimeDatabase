55bccc96ba925e24f3e3525c546043a8ed8e3df4
Huyen2402 <huyenb1910384@student.ctu.edu.vn>
2022-11-11 10:42:42 +0700

    Add project files.

---
+++ RealtimeDatabase/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealtimeDatabase
+{
+     class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
---
+++ RealtimeDatabase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base;
+using TableDependency.SqlClient.Base.Enums;
+namespace RealtimeDatabase
+{
+    class Program
+
+    { 
+        static void Main(string[] args)
+        {
+           
+            using (var tbDenpendency = new SqlTableDependency<Person>("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RealTime;Integrated Security=True"))
+            {
+                tbDenpendency.OnChanged += TbDenpendency_OnChanged;
+                tbDenpendency.Start();
+                Console.ReadKey();
+               
+            }
+
+               
+        }
+
+        private static void TbDenpendency_OnChanged(object sender, TableDependency.SqlClient.Base.EventArgs.RecordChangedEventArgs<Person> e)
+        {
+            if(e.ChangeType != ChangeType.None)
+            {
+                var entry = e.Entity;
+                Console.WriteLine("Type:" + e.ChangeType);
+                Console.WriteLine("ID: " + entry.Id + "Name: " + entry.Name + "Gender: " + entry.Gender);
+                
+            }
+        }
+    }
+}
